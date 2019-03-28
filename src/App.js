8e828533c2bdd2a0319f1d8ffbf75074e641d91f
Kyle Butler <kylbutlr@gmail.com>
2019-03-27 17:28:40 -0700

    mobile vh fix

--- src/App.js
+++ src/App.js
@@ -78,6 +78,7 @@ class App extends Component {
   }
 
   componentDidMount() {
+    this.runResize();
     this.setState(
       {
         notes: [],
@@ -87,6 +88,15 @@ class App extends Component {
         this.getSavedSession();
       }
     );
+  }
+
+  runResize() {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+    window.addEventListener('resize', () => {
+      let vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    });
   }
 
   tabClick(activeTab) {
